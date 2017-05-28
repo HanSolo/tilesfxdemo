8a8003bb7b851b8ebbc2c73e4e8e854e1411907f
Gerrit Grunwald <han.solo@mac.com>
2017-05-28 06:18:42 +0200

    Some modifications related to TilesFX 1.4.3

--- src/main/java/eu/hansolo/tilesfxdemo/Overview.java
+++ src/main/java/eu/hansolo/tilesfxdemo/Overview.java
@@ -28,32 +28,28 @@ import eu.hansolo.tilesfx.tools.Location;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.geometry.Insets;
-import javafx.geometry.Orientation;
+import javafx.scene.Scene;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Stop;
 import javafx.stage.Stage;
-import javafx.scene.Scene;
 import org.kordamp.ikonli.javafx.FontIcon;
 
 import java.time.LocalTime;
 import java.util.Locale;
 import java.util.Random;
 
-import static org.kordamp.ikonli.weathericons.WeatherIcons.*;
+import static org.kordamp.ikonli.weathericons.WeatherIcons.SNOW;
 
 
 /**
- * User: hansolo
- * Date: 08.01.17
- * Time: 07:33
+ * Created by hansolo on 28.05.17.
  */
-public class Main extends Application {
+public class Overview extends Application {
     private static final double TILE_SIZE = 150;
     private static final Random RND       = new Random();
 
@@ -92,34 +88,7 @@ public class Main extends Application {
     private Tile              donutChartTile;
     private Tile              circularProgressTile;
     private Tile              stockTile;
-    private FontIcon          icon;
-    private Tile              ikonliTile;
-    private Gauge             indicatorGauge;
-    private Tile              indicatorTile;
-    private Gauge             slimGauge;
-    private Tile              slimTile;
-    private Gauge             dashboardGauge;
-    private Tile              dashboardTile;
-    private Gauge             digitalGauge;
-    private Tile              digitalTile;
-    private Gauge             simpleDigitalGauge;
-    private Tile              simpleDigitalTile;
-    private Gauge             simpleSectionGauge;
-    private Tile              simpleSectionTile;
-    private Gauge             bulletChartGauge;
-    private Tile              bulletChartTile;
-    private Gauge             spaceXGauge;
-    private Tile              spaceXTile;
-    private Clock             slimClock;
-    private Tile              slimClockTile;
-    private Regulator         regulator;
-    private Tile              regulatorTile;
-    private FeedbackRegulator feedbackRegulator;
-    private Tile              feedbackRegulatorTile;
-    private ColorRegulator    colorRegulator;
-    private Tile              colorRegulatorTile;
-    //private RestClient        aaplRestClient;
-    //private long              lastStockCall;
+    private Tile              gaugeSparkLineTile;
     private long              lastTimerCall;
     private AnimationTimer    timer;
 
@@ -400,13 +369,13 @@ public class Main extends Application {
                                      .build();
 
         donutChartTile = TileBuilder.create()
-                                     .skinType(SkinType.DONUT_CHART)
-                                     .prefSize(TILE_SIZE, TILE_SIZE)
-                                     .title("DonutChart Tile")
-                                     .text("Whatever text")
-                                     .textVisible(false)
-                                     .radialChartData(chartData1, chartData2, chartData3, chartData4)
-                                     .build();
+                                    .skinType(SkinType.DONUT_CHART)
+                                    .prefSize(TILE_SIZE, TILE_SIZE)
+                                    .title("DonutChart Tile")
+                                    .text("Whatever text")
+                                    .textVisible(false)
+                                    .radialChartData(chartData1, chartData2, chartData3, chartData4)
+                                    .build();
 
         circularProgressTile = TileBuilder.create()
                                           .skinType(SkinType.CIRCULAR_PROGRESS)
@@ -425,156 +394,28 @@ public class Main extends Application {
                                .textVisible(false)
                                .build();
 
-        icon = new FontIcon(SNOW);
-        icon.setIconSize((int) TILE_SIZE);
-        icon.setFill(Tile.FOREGROUND);
-        ikonliTile = TileBuilder.create()
-                                .skinType(SkinType.CUSTOM)
-                                .prefSize(TILE_SIZE, TILE_SIZE)
-                                .title("Ikonli Icon")
-                                .graphic(icon)
-                                .text("Snow")
-                                .build();
-
-        slimGauge = createGauge(Gauge.SkinType.SLIM);
-        slimTile  = TileBuilder.create()
-                               .prefSize(TILE_SIZE, TILE_SIZE)
-                               .skinType(SkinType.CUSTOM)
-                               .title("Medusa Slim")
-                               .text("Temperature")
-                               .graphic(slimGauge)
-                               .build();
-
-        dashboardGauge = createGauge(Gauge.SkinType.DASHBOARD);
-        dashboardTile  = TileBuilder.create()
-                                    .prefSize(TILE_SIZE, TILE_SIZE)
-                                    .skinType(SkinType.CUSTOM)
-                                    .title("Medusa Dashboard")
-                                    .text("Temperature")
-                                    .graphic(dashboardGauge)
-                                    .build();
-
-        digitalGauge = createGauge(Gauge.SkinType.DIGITAL);
-        digitalTile  = TileBuilder.create()
-                                  .prefSize(TILE_SIZE, TILE_SIZE)
-                                  .skinType(SkinType.CUSTOM)
-                                  .title("Medusa Digital")
-                                  .text("Temperature")
-                                  .graphic(digitalGauge)
-                                  .build();
-
-        simpleDigitalGauge = createGauge(Gauge.SkinType.SIMPLE_DIGITAL);
-        simpleDigitalTile  = TileBuilder.create()
+        gaugeSparkLineTile = TileBuilder.create()
+                                        .skinType(SkinType.GAUGE_SPARK_LINE)
                                         .prefSize(TILE_SIZE, TILE_SIZE)
-                                        .skinType(SkinType.CUSTOM)
-                                        .title("Medusa SimpleDigital")
-                                        .text("Temperature")
-                                        .graphic(simpleDigitalGauge)
-                                        .build();
-
-        indicatorGauge = createGauge(Gauge.SkinType.INDICATOR);
-        indicatorTile  = TileBuilder.create()
-                                    .prefSize(TILE_SIZE, TILE_SIZE)
-                                    .skinType(SkinType.CUSTOM)
-                                    .title("Medusa Indicator")
-                                    .text("")
-                                    .graphic(indicatorGauge)
-                                    .build();
-        indicatorTile.textProperty().bind(indicatorGauge.currentValueProperty().asString("%.0f"));
-
-        simpleSectionGauge = createGauge(Gauge.SkinType.SIMPLE_SECTION);
-        simpleSectionGauge.setBarColor(Tile.FOREGROUND);
-        simpleSectionGauge.setSections(new Section(66, 100, Tile.BLUE));
-        simpleSectionTile  = TileBuilder.create()
-                                        .prefSize(TILE_SIZE, TILE_SIZE)
-                                        .skinType(SkinType.CUSTOM)
-                                        .title("Medusa SimpleSection")
-                                        .text("Temperature")
-                                        .graphic(simpleSectionGauge)
-                                        .build();
-
-        bulletChartGauge = createGauge(Gauge.SkinType.BULLET_CHART);
-        bulletChartGauge.setThreshold(75);
-        bulletChartGauge.setBarColor(Tile.FOREGROUND);
-        bulletChartGauge.setThresholdColor(Tile.RED);
-        bulletChartGauge.setSections(new Section(0, 33, Tile.BLUE.darker().darker()),
-                                     new Section(33, 66, Tile.BLUE.darker()),
-                                     new Section(66, 100, Tile.BLUE));
-        bulletChartTile = TileBuilder.create()
-                                     .prefSize(TILE_SIZE, TILE_SIZE)
-                                     .skinType(SkinType.CUSTOM)
-                                     .title("Medusa BulletChart")
-                                     .text("Temperature")
-                                     .graphic(bulletChartGauge)
-                                     .build();
-
-        slimClock = ClockBuilder.create()
-                                .prefSize(TILE_SIZE, TILE_SIZE)
-                                .skinType(ClockSkinType.SLIM)
-                                .secondColor(Tile.FOREGROUND)
-                                .minuteColor(Tile.BLUE)
-                                .hourColor(Tile.FOREGROUND)
-                                .dateColor(Tile.FOREGROUND)
-                                .running(true)
-                                .build();
-        slimClockTile = TileBuilder.create()
-                                   .prefSize(TILE_SIZE, TILE_SIZE)
-                                   .skinType(SkinType.CUSTOM)
-                                   .title("Medusa Clock")
-                                   .graphic(slimClock)
-                                   .textVisible(false)
-                                   .build();
-
-        spaceXGauge = createGauge(Gauge.SkinType.SPACE_X);
-        spaceXGauge.setThresholdColor(Tile.BLUE);
-        spaceXGauge.setThreshold(85);
-        spaceXGauge.setBarColor(Tile.FOREGROUND);
-        spaceXTile = TileBuilder.create()
-                                .prefSize(TILE_SIZE, TILE_SIZE)
-                                .skinType(SkinType.CUSTOM)
-                                .title("Medusa SpaceX")
-                                .text("Temperature")
-                                .graphic(spaceXGauge)
-                                .build();
-
-        regulator = RegulatorBuilder.create()
-                                    .prefSize(TILE_SIZE, TILE_SIZE)
-                                    .color(Tile.BACKGROUND)
-                                    .barColor(Tile.BLUE)
-                                    .textColor(Tile.FOREGROUND)
-                                    .build();
-        regulatorTile = TileBuilder.create()
-                                   .prefSize(TILE_SIZE, TILE_SIZE)
-                                   .skinType(SkinType.CUSTOM)
-                                   .title("Regulator")
-                                   .text("Light")
-                                   .graphic(regulator)
-                                   .build();
-
-        feedbackRegulator = FeedbackRegulatorBuilder.create()
-                                                    .prefSize(TILE_SIZE, TILE_SIZE)
-                                                    .color(Tile.BACKGROUND)
-                                                    .textColor(Tile.FOREGROUND)
-                                                    .build();
-        feedbackRegulatorTile = TileBuilder.create()
-                                    .prefSize(TILE_SIZE, TILE_SIZE)
-                                    .skinType(SkinType.CUSTOM)
-                                    .title("FeedbackRegulator")
-                                    .text("Temp")
-                                    .graphic(feedbackRegulator)
-                                    .build();
-
-        colorRegulator = ColorRegulatorBuilder.create()
-                                              .prefSize(TILE_SIZE, TILE_SIZE)
-                                              .color(Tile.BACKGROUND)
-                                              .textColor(Tile.FOREGROUND)
-                                              .build();
-        colorRegulatorTile = TileBuilder.create()
-                                        .prefSize(TILE_SIZE, TILE_SIZE)
-                                        .skinType(SkinType.CUSTOM)
-                                        .title("Regulator")
-                                        .text("Light")
-                                        .graphic(colorRegulator)
+                                        .title("GaugeSparkLine Tile")
+                                        .animated(true)
+                                        .textVisible(false)
+                                        .averagingPeriod(25)
+                                        .autoReferenceValue(true)
+                                        .barColor(Tile.YELLOW_ORANGE)
+                                        .barBackgroundColor(Color.rgb(255, 255, 255, 0.1))
+                                        .sections(new eu.hansolo.tilesfx.Section(0, 33, Tile.LIGHT_GREEN),
+                                                  new eu.hansolo.tilesfx.Section(33, 67, Tile.YELLOW),
+                                                  new eu.hansolo.tilesfx.Section(67, 100, Tile.LIGHT_RED))
+                                        .sectionsVisible(true)
+                                        .highlightSections(true)
+                                        .strokeWithGradient(true)
+                                        .gradientStops(new Stop(0.0, Tile.LIGHT_GREEN),
+                                                       new Stop(0.33, Tile.LIGHT_GREEN),
+                                                       new Stop(0.33,Tile.YELLOW),
+                                                       new Stop(0.67, Tile.YELLOW),
+                                                       new Stop(0.67, Tile.LIGHT_RED),
+                                                       new Stop(1.0, Tile.LIGHT_RED))
                                         .build();
 
         //lastStockCall = System.nanoTime();
@@ -600,54 +441,26 @@ public class Main extends Application {
 
                     stockTile.setValue(RND.nextDouble() * RND.nextDouble() * 150 + RND.nextDouble() * 10);
 
+                    gaugeSparkLineTile.setValue(RND.nextDouble() * 100);
+
                     chartData1.setValue(RND.nextDouble() * 50);
                     chartData2.setValue(RND.nextDouble() * 50);
                     chartData3.setValue(RND.nextDouble() * 50);
                     chartData4.setValue(RND.nextDouble() * 50);
 
-                    slimGauge.setValue(RND.nextDouble() * 100);
-                    dashboardGauge.setValue(RND.nextDouble() * 100);
-                    digitalGauge.setValue(RND.nextDouble() * 100);
-                    simpleDigitalGauge.setValue(RND.nextDouble() * 100);
-                    indicatorGauge.setValue(RND.nextDouble() * 100);
-                    simpleSectionGauge.setValue(RND.nextDouble() * 100);
-                    bulletChartGauge.setValue(RND.nextDouble() * 100);
-                    spaceXGauge.setValue(RND.nextDouble() * 100);
-
-                    // "Control" feedback regulator
-                    double currentValue = feedbackRegulator.getCurrentValue();
-                    double targetValue  = feedbackRegulator.getTargetValue();
-                    if ((int) currentValue != (int) targetValue) {
-                        if (currentValue < targetValue) {
-                            feedbackRegulator.setCurrentValue(currentValue+1);
-                        } else if (currentValue > targetValue) {
-                            feedbackRegulator.setCurrentValue(currentValue-1);
-                        }
-                    }
 
                     lastTimerCall = now;
                 }
-                //if (now > lastStockCall + 300_000_000_000l) {
-                //    aaplRestClient.updateQuote();
-                //    lastStockCall = now;
-                //}
             }
         };
-
-        //aaplRestClient.quoteProperty().addListener(o -> stockTile.setValue(Double.parseDouble(aaplRestClient.getQuote().get("LastPrice").toString())));
-        //aaplRestClient.updateQuote();
     }
 
     @Override public void start(Stage stage) {
         FlowGridPane pane = new FlowGridPane(6, 6, percentageTile, clockTile, gaugeTile, sparkLineTile, areaChartTile,
-                                         lineChartTile, timerControlTile, numberTile, textTile,
-                                         highLowTile, plusMinusTile, sliderTile, switchTile, worldTile, timeTile,
-                                         barChartTile, customTile, leaderBoardTile, mapTile, radialChartTile,
-                                         donutChartTile, circularProgressTile, stockTile,
-                                         ikonliTile, slimTile, dashboardTile, digitalTile,
-                                         simpleDigitalTile, indicatorTile, simpleSectionTile,
-                                         bulletChartTile, slimClockTile, spaceXTile,
-                                         regulatorTile, feedbackRegulatorTile, colorRegulatorTile);
+                                             lineChartTile, timerControlTile, numberTile, textTile,
+                                             highLowTile, plusMinusTile, sliderTile, switchTile, worldTile, timeTile,
+                                             barChartTile, customTile, leaderBoardTile, mapTile, radialChartTile,
+                                             donutChartTile, circularProgressTile, stockTile, gaugeSparkLineTile);
         pane.setHgap(5);
         pane.setVgap(5);
         pane.setPadding(new Insets(5));
@@ -655,32 +468,11 @@ public class Main extends Application {
 
         Scene scene = new Scene(pane);
 
-        stage.setTitle("TilesFX Dashboard using Medusa");
+        stage.setTitle("TilesFX");
         stage.setScene(scene);
         stage.show();
 
         timer.start();
-    }
-
-    private Gauge createGauge(final Gauge.SkinType TYPE) {
-        return GaugeBuilder.create()
-                           .skinType(TYPE)
-                           .prefSize(TILE_SIZE, TILE_SIZE)
-                           .animated(true)
-                           //.title("")
-                           .unit("\u00B0C")
-                           .valueColor(Tile.FOREGROUND)
-                           .titleColor(Tile.FOREGROUND)
-                           .unitColor(Tile.FOREGROUND)
-                           .barColor(Tile.BLUE)
-                           .needleColor(Tile.FOREGROUND)
-                           .barColor(Tile.BLUE)
-                           .barBackgroundColor(Tile.BACKGROUND.darker())
-                           .tickLabelColor(Tile.FOREGROUND)
-                           .majorTickMarkColor(Tile.FOREGROUND)
-                           .minorTickMarkColor(Tile.FOREGROUND)
-                           .mediumTickMarkColor(Tile.FOREGROUND)
-                           .build();
     }
 
     @Override public void stop() {
